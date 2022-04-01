7aacfe6d89efc945477ac678bbbf691cae5dd405
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2022-03-31 23:12:51 -0300

    requisito 2 completo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route path="*" component={ Login } />
+        <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
       </Switch>
     </div>
